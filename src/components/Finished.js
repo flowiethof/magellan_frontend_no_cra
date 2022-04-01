f0fe2f0bf7b214c556f89f1330c3b1d34cd16605
Florian Wiethof <fwiethof@Florians-MacBook-Pro-2.local>
2022-04-01 11:38:30 +0200

    Added finished page

--- src/components/Finished.js
+++ src/components/Finished.js
@@ -1,7 +1,9 @@
 import React from "react";
+import {Party} from "../img"
 
 export function Finished() {
   return <>
-  
+    <h4>Nice, nothing more to screen here!</h4>
+    <img src={Party} />
   </>;
 }
--- src/img/index.js
+++ src/img/index.js
@@ -1,5 +1,6 @@
 import Olli from "./olli.png"
 import LoadingGif from "./loading.gif"
 import Logo from "./picus_logo.jpg"
+import Party from "./party.gif"
 
-export {Olli, LoadingGif, Logo}
+export {Olli, LoadingGif, Logo, Party}
