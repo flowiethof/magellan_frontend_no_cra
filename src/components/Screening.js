22edd0c8bbc08fbc9f43d96888a21095c8555314
Florian Wiethof <fwiethof@Florians-MacBook-Pro-2.local>
2022-04-07 16:04:45 +0200

    Formatted frontend

--- src/components/Screening.js
+++ src/components/Screening.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { get_gsheet_data, write_to_gsheet, convert_sheet_to_objects, convert_objects_to_sheet } from "./GSheet";
+import {
+  get_gsheet_data,
+  write_to_gsheet,
+  convert_sheet_to_objects,
+  convert_objects_to_sheet,
+} from "./GSheet";
 
 const sheet = "1ongBRK_4CCyRG0YW21Wo4f8zEX8gNB7pfD49obuGx4A";
 
-
-
 const beautify = {
   Category: "Category",
-  funded_organization_description: "Description",
+  description: "Description",
+  URL: "URL",
+  money_raised: "Round size",
   funded_organization_funding_total: "Total funding",
   funded_organization_location: "Location",
-  investor_identifiers: "Investors",
   lead_investor_identifiers: "Lead Investors",
+  investor_identifiers: "Investors",
   announced_on: "Announced on",
   investment_type: "Round stage",
-  founded_on: "Founded on",
+  founded_on: "Company age",
   categories: "Categories",
-  URL: "URL",
+  website_url: "Website URL",
 };
-
-
 
 export function Screening(props) {
   const [data, setData] = useState(false);
@@ -29,14 +32,32 @@ export function Screening(props) {
   const { setPage, category } = props;
 
   useEffect(() => {
-    get_gsheet_data(sheet, "screened_companies!A1:N300", (res) => {
-      get_gsheet_data(sheet, "all_companies_filtered!A1:N300", (_res) => {
+    get_gsheet_data(sheet, "screened_companies!A1:P300", (res) => {
+      get_gsheet_data(sheet, "all_companies_filtered!A1:P300", (_res) => {
         let ex_temp = convert_sheet_to_objects(res, category, "all");
-        console.log(ex_temp)
+        console.log(ex_temp);
         let new_temp = convert_sheet_to_objects(_res, category, "relevant");
-        console.log(new_temp)
+        console.log(new_temp);
         let temp = ex_temp.map((e) => e["URL"]);
         temp = new_temp.filter((e) => !temp.includes(e["URL"]));
+        temp = temp.filter((e) => parseInt(e["founded_on"]) < 10);
+        temp.forEach((e) => {
+          let funding_total = e["funded_organization_funding_total"];
+          e["funded_organization_funding_total"] =
+            Math.round((funding_total / 1e6) * 100) / 100 + "M";
+          let round_size = e["money_raised"];
+          e["money_raised"] = Math.round((round_size / 1e6) * 100) / 100 + "M";
+          let founded_on = e["founded_on"];
+          if (founded_on === "1") {
+            e["founded_on"] = "1 year";
+          } else if (founded_on === 0) {
+            e["founded_on"] = "<1 year";
+          } else {
+            e["founded_on"] = founded_on + " years";
+          }
+          e["funded_organization_location"] =
+            e["funded_organization_location"].split(",")[2];
+        });
         setData(temp);
       });
     });
@@ -57,8 +78,13 @@ export function Screening(props) {
 
     document.getElementById("responsible").value = "";
     document.getElementById("comment").value = "";
-
-    write_to_gsheet(sheet, "screened_companies!A:A", convert_objects_to_sheet([this_temp]) );
+    console.log(this_temp);
+    console.log(convert_objects_to_sheet([this_temp]));
+    write_to_gsheet(
+      sheet,
+      "screened_companies!A:A",
+      convert_objects_to_sheet([this_temp])
+    );
   };
 
   if (index === data.length) {
@@ -66,42 +92,145 @@ export function Screening(props) {
   } else {
     if (data) {
       Object.keys(beautify).forEach((key) => {
-        if (!["Category", "Link", "funded_organization_identifier", "category_groups", ].includes(key) ) {
-          if (beautify[key] === "Description") {
-            rows.push(<tr key={key} style={{ backgroundColor: "lightgray" }}> <th>{beautify[key]}</th> <td>{data[index][key]}</td></tr>);
-          } else {
-            rows.push( <tr key={key}> <th>{beautify[key]}</th> <td>{data[index][key]}</td> </tr> );
+        if (
+          ![
+            "Category",
+            "Link",
+            "funded_organization_identifier",
+            "category_groups",
+            "announced_on",
+            "investment_type",
+            "website_url",
+            "money_raised",
+          ].includes(key)
+        ) {
+          switch (beautify[key]) {
+            case "Description":
+              rows.push(
+                <tr key={key} style={{ backgroundColor: "lightgray" }}>
+                  <th>{beautify[key]}</th> <td>{data[index][key]}</td>
+                </tr>
+              );
+              break;
+            case "URL":
+              rows.push(
+                <tr key={key}>
+                  <th>Websites</th>
+                  <td>
+                    <a href="#" onClick={() => window.open(data[index][key])}>
+                      Crunchbase
+                    </a>
+                    {" - "}
+                    <a
+                      href="#"
+                      onClick={() => window.open(data[index]["website_url"])}
+                    >
+                      Website
+                    </a>
+                  </td>
+                </tr>
+              );
+              break;
+            case "Total funding":
+              rows.push(
+                <tr key={key}>
+                  <th>Funding</th>
+                  <td>
+                    <b>Round:</b> {data[index]["money_raised"]}
+                    <br />
+                    <b>Total:</b> {data[index][key]}
+                  </td>
+                </tr>
+              );
+              break;
+            default:
+              rows.push(
+                <tr key={key}>
+                  {" "}
+                  <th>{beautify[key]}</th> <td>{data[index][key]}</td>{" "}
+                </tr>
+              );
           }
         }
       });
     }
   }
 
-  const team = ["", "Moritz", "Carina", "Stephan", "Florian", "Alex", "Philipp", "Lukas", "Niclas", "Sebastian", "Olli", "Kolja", "Mikko", "Paulina", "Leonie", "Lauritz"].sort().map(e => <option key={e}>{e}</option>)
+  const team = [
+    "",
+    "Moritz",
+    "Carina",
+    "Stephan",
+    "Florian",
+    "Alex",
+    "Philipp",
+    "Lukas",
+    "Niclas",
+    "Sebastian",
+    "Olli",
+    "Kolja",
+    "Mikko",
+    "Paulina",
+    "Leonie",
+    "Lauritz",
+  ]
+    .sort()
+    .map((e) => <option key={e}>{e}</option>);
 
   return (
-    <>
-      {data && index < data.length && (
-        <>
-          <h3 style={{ marginBottom: "30px" }}>
-            <a href={data[index]["URL"]} style={{ color: "gray" }}>{data[index]["funded_organization_identifier"]}</a>
-          </h3>
-          <h5>{index + 1}/{data.length}</h5>
-        </>
-      )}
-      <table>{rows}</table>
-      <form id="profile-form">
-        <div>
-          <h4>Comment</h4>
-          <textarea id="comment"></textarea>
+    data &&
+    index < data.length && (
+      <>
+        <div className="row">
+          <div className="col-sm-6">
+            <iframe
+              src={data[index]["website_url"]}
+              style={{ width: "100%", height: "100%" }}
+            ></iframe>
+          </div>
+          <div className="col-sm-6">
+            <>
+              <h3 style={{ marginBottom: "30px" }}>
+                <a
+                  href="#"
+                  onClick={() => window.open(data[index]["URL"])}
+                  style={{ color: "gray" }}
+                >
+                  {data[index]["funded_organization_identifier"]}
+                </a>
+              </h3>
+              <h5>
+                {index + 1}/{data.length}
+              </h5>
+            </>
+            <table>{rows}</table>
+            <form id="profile-form">
+              <div>
+                <h4>Comment</h4>
+                <textarea id="comment"></textarea>
+              </div>
+              <div>
+                <h4>Responsible</h4>
+                <select id="responsible">{team}</select>
+              </div>
+              <button
+                className="btn btn-success"
+                type="button"
+                onClick={() => handleSubmit("relevant")}
+              >
+                Relevant
+              </button>
+              <button
+                className="btn btn-secondary"
+                type="button"
+                onClick={() => handleSubmit("reject")}
+              >
+                Reject
+              </button>
+            </form>
+          </div>
         </div>
-        <div>
-          <h4>Responsible</h4>
-          <select id="responsible">{team}</select>
-        </div>
-        <button className="btn btn-success" type="button" onClick={() => handleSubmit("relevant")} >Relevant</button>
-        <button className="btn btn-secondary" type="button" onClick={() => handleSubmit("reject")} >Reject</button>
-      </form>
-    </>
-  )
+      </>
+    )
+  );
 }
